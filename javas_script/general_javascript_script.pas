073f93fcc33efe75815f8312bab5b40bec457c6c
Chris <ulrich@tcs-germany.de>
2016-02-23 15:17:20 +0100

    add javascript component

---
+++ javas_script/general_javascript_script.pas
@@ -0,0 +1,20 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit general_javascript_script;
+
+interface
+
+uses
+  genjavascriptscript, BESEN, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('general_javascript_script', @Register);
+end.
---
+++ javas_script/genjavascriptscript.pas
@@ -0,0 +1,119 @@
+{*******************************************************************************
+  Copyright (C) Christian Ulrich info@cu-tec.de
+
+  This source is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2 of the License, or commercial alternative
+  contact us for more information
+
+  This code is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  A copy of the GNU General Public License is available on the World Wide Web
+  at <http://www.gnu.org/copyleft/gpl.html>. You can also obtain it by writing
+  to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+  MA 02111-1307, USA.
+Created 07.10.2015
+*******************************************************************************}
+unit genjavascriptscript;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Utils ,genscript,BESEN;
+
+type
+  { TPythonScript }
+
+  TJavascriptScript = class(TScript)
+  private
+    FLines : TStringList;
+    FRunning: Boolean;
+    FStopping : Boolean;
+  protected
+    function GetTyp: string; override;
+  public
+    procedure Init; override;
+    function IsRunning: Boolean; override;
+    function GetStatus: TScriptStatus; override;
+    function Stop: Boolean; override;
+    function Execute(aParameters: Variant;Debug : Boolean = false): Boolean; override;
+    destructor Destroy; override;
+  end;
+
+implementation
+
+var
+  aScript : TJavascriptScript;
+
+function TJavascriptScript.GetTyp: string;
+begin
+  Result := 'JavaScript';
+end;
+
+procedure TJavascriptScript.Init;
+begin
+  FLines:=nil;
+  FRunning:=False;
+  FStopping:=False;
+end;
+
+function TJavascriptScript.IsRunning: Boolean;
+begin
+  Result:=FRunning;
+end;
+
+function TJavascriptScript.GetStatus: TScriptStatus;
+begin
+  if IsRunning then
+    Result := ssRunning
+  else Result := ssNone;
+end;
+
+function TJavascriptScript.Stop: Boolean;
+begin
+  if IsRunning then
+    begin
+      FStopping := True;
+      Result:=True;
+    end;
+end;
+
+function TJavascriptScript.Execute(aParameters: Variant; Debug: Boolean): Boolean;
+var
+  aLine: Integer=0;
+  aPosition : Integer=0;
+  i: Integer;
+begin
+  Result := False;
+  try
+    FRunning := True;
+    //Exec
+    FRunning:=False;
+    Result := True;
+  except
+    on e : Exception do
+      begin
+        if Assigned(Writeln) then
+          Writeln(e.Message);
+        FRunning:=False;
+        FreeAndNil(FLines);
+        Init;
+        FStopping:=False;
+      end;
+  end;
+end;
+
+destructor TJavascriptScript.Destroy;
+begin
+  inherited Destroy;
+end;
+
+initialization
+
+end.
+
