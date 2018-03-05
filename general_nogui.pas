d00d41c3b95309221d218ea9ea6a50c8928451a4
Chris <ulrich@tcs-germany.de>
2018-03-05 11:53:02 +0100

    add dependency to avammdblayer

--- general_nogui.pas
+++ general_nogui.pas
@@ -4,14 +4,12 @@
 
 unit general_nogui;
 
-{$warn 5023 off : no warning about unused units}
 interface
 
 uses
   Utils, uGeneralStrConsts, SecureUtils, umashineid, uModifiedDS, uRTFtoTXT, 
   MathParser, Aspell, usimpleprocess, uSunRise, UtilsDate, uminiconvencoding, 
-  uEncrypt, uBaseDatasetInterfaces, pwgenerator, genscript, comparewild, 
-  uhttputil;
+  uEncrypt, pwgenerator, genscript, comparewild, uhttputil;
 
 implementation
 
